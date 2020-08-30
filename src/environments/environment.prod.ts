edff7c6f3358424c38adf4c8d100f6ae39af58da
arifkhoja <37096407+arifkhoja@users.noreply.github.com>
2020-08-30 12:07:54 +0530

    added src

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+// Include your Firebase configuration details.
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  }
+};
